7c98654481f05bf23f1b580dfaed33cd51585e69
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-03-14 21:50:49 +0530

    Add transaction

--- src/commands/faucet.command.js
+++ src/commands/faucet.command.js
@@ -1,7 +1,11 @@
 const Command = require("../structures/command.structure.js");
-const Web3 = require("web3");
-const Tx = require('ethereumjs-tx');
+require("log-timestamp");
+const ethers = require("ethers");
+const dotenv = require("dotenv");
+dotenv.config();
 
+const privateKey = process.env.PRIVATE_KEY.toString("hex");
+const wallet = new ethers.Wallet(privateKey);
 
 module.exports = new Command({
   name: "faucet",
@@ -9,8 +13,37 @@ module.exports = new Command({
   description: "Faucet huh.",
 
   async run(msg, args, client) {
+    const address = wallet.address;
+    console.log(args);
 
+    const httpsUrl = process.env.ALCHEMY_API_URL;
 
-    await msg.reply(`This is my bruh faucet!`);
+    const init = async function () {
+      const httpsProvider = new ethers.getDefaultProvider(httpsUrl);
+
+      let nonce = await httpsProvider.getTransactionCount(address);
+
+      let feeData = await httpsProvider.getFeeData();
+
+      const tx = {
+        type: 2,
+        nonce: nonce,
+        to: args[1],
+        maxPriorityFeePerGas: feeData["maxPriorityFeePerGas"],
+        maxFeePerGas: feeData["maxFeePerGas"],
+        value: ethers.utils.parseEther("1"),
+        gasLimit: "21000",
+        chainId: 80001,
+      };
+
+      const signedTx = await wallet.signTransaction(tx);
+
+      const txHash = ethers.utils.keccak256(signedTx);
+      console.log("Precomputed txHash:", txHash);
+      await msg.reply(`https://mumbai.polygonscan.com/tx/${txHash}`);
+      httpsProvider.sendTransaction(signedTx).then(console.log);
+    };
+
+    init();
   },
 });
